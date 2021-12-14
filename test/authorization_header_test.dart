fcea5b2a026c8d003c8c11ab296debde78afcd3f
Kato Shinya <kato.shinya.dev@gmail.com>
2021-12-14 09:33:26 +0900

    Supported more multiple platforms.

--- test/authorization_header_test.dart
+++ test/authorization_header_test.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // Package imports:
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 // Project imports:
 import 'package:authorization_header/authorization_header.dart';
